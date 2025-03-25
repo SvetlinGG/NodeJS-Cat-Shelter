b57a800a1b76a800e80fc4a39fa40d788a8c28c9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-25 01:14:47 +0100

    dynamic data

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const http = require('http');
 
-const indexHtml = require('./views/home/index.html');
+const indexTemplate = require('./views/home/index.html');
 const siteCss = require('./content/styles/site.css');
 const addBreedHtml = require('./views/addBreed.html');
 const addCatHtml = require('./views/addCat.html');
 
-const port = 2000
+
+const port = 2000;
+
+const cats = [
+    {
+        id: 1,
+        imageUrl: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="Black Cat',
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 2,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg',
+        name: 'Sunny',
+        breed: 'Angora',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 3,
+        imageUrl:"https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg",
+        name: 'Murry',
+        breed: 'Persian',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 4,
+        imageUrl: "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+        name: 'Navcho',
+        breed: 'Sphinks',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 5,
+        imageUrl: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+        name: 'Sunny',
+        breed: 'Nqkakva',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    }
+];
 
 const server = http.createServer((req, res) => {
 
@@ -21,7 +60,7 @@ const server = http.createServer((req, res) => {
     
         switch (req.url) {
             case '/':
-                res.write(indexHtml);
+                res.write(indexTemplate(cats));
                 break;
             case '/cats/add-breed':
                 res.write(addBreedHtml);
